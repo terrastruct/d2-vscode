46f9dc0d4804e3bd0d1ea4a82540c5ea7f822c52
Barry Nolte <barry@barrynolte.com>
2023-01-31 14:23:21 -0800

    Show/Hide Tala option on install status

--- src/layoutPicker.ts
+++ src/layoutPicker.ts
@@ -1,4 +1,6 @@
 import { QuickPickItem, window } from "vscode";
+import { util } from "./utility";
+import * as os from "os";
 
 /**
  * Container for D2 Layouts
@@ -18,15 +20,33 @@ class LayoutItem implements QuickPickItem {
  */
 const layouts: QuickPickItem[] = [
   new LayoutItem("dagre", "The directed graph layout library Dagre"),
-  new LayoutItem("elk", "Eclipse Layout Kernel (ELK) with the Layered algorithm"),
-  new LayoutItem("tala", "General Orthogonal Layout Engine")
+  new LayoutItem("elk", "Eclipse Layout Kernel (ELK) with the Layered algorithm")
 ];
+
+const layoutTala = new LayoutItem("tala", "General Orthogonal Layout Engine");
+
+const talaPluginName: string = process.platform === 'win32' ? 'd2plugin-tala.dll' : 'd2plugin-tala'; 
 
 /**
  * layouPicker - This will show the quick pick list in
  * the command pallette when called
  */
 export class layoutPicker {
+  constructor() {
+    // If the plugin file exists, add the option if it hasn't been added before
+    if (util.isFileOnPath(talaPluginName)) {
+      if (layouts.indexOf(layoutTala) === -1) {
+        layouts.push(layoutTala);
+      }
+    } else {
+      // If the plugin file does *not* exist, remove the option if it exists in the array
+      const idx = layouts.indexOf(layoutTala);
+      if (idx !== -1) {
+        layouts.splice(idx, 1);
+      }
+    }
+  }
+
   showPicker(): Thenable<QuickPickItem | undefined> {
     return window.showQuickPick(layouts, {
       title: "Layouts",
