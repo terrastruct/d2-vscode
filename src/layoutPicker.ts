842f73e10d223c864ba5d1d48a06b15a845bfde8
Barry Nolte <barry@barrynolte.com>
2023-01-30 20:56:18 -0800

    Added Tala to quickPick

--- src/layoutPicker.ts
+++ src/layoutPicker.ts
@@ -18,10 +18,8 @@ class LayoutItem implements QuickPickItem {
  */
 const layouts: QuickPickItem[] = [
   new LayoutItem("dagre", "The directed graph layout library Dagre"),
-  new LayoutItem(
-    "elk",
-    "Eclipse Layout Kernel (ELK) with the Layered algorithm"
-  ),
+  new LayoutItem("elk", "Eclipse Layout Kernel (ELK) with the Layered algorithm"),
+  new LayoutItem("tala", "General Orthogonal Layout Engine")
 ];
 
 /**
